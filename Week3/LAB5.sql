d67677e24405d0881a5d5a9acfc99f5614581feb
Wizh232 <118735130+Wizh232@users.noreply.github.com>
2023-04-05 11:06:03 +0700

    Add files via upload

---
+++ Week3/LAB5.sql
@@ -0,0 +1,17 @@
+﻿--Câu 3
+CREATE FUNCTION cau3_lab5(@TenSanPham NVARCHAR(255), @Nam INT)
+RETURNS INT
+AS
+BEGIN
+    DECLARE @TongSoLuong INT;
+    SELECT @TongSoLuong = SUM(soluongN) - SUM(soluongX)
+    FROM Nhap n
+    INNER JOIN Xuat x ON n.masp = x.masp
+    INNER JOIN Sanpham sp ON n.masp = sp.masp
+    WHERE sp.tensp = @TenSanPham AND YEAR(n.ngaynhap) = @Nam AND YEAR(x.ngayxuat) = @Nam;
+    RETURN @TongSoLuong;
+END;
+
+DECLARE @KetQua INT;
+EXEC @KetQua = cau3_lab5 'F3 lite', 2020;
+SELECT @KetQua AS 'Tổng số lượng thay đổi';
